0f1d4265bf306bc1b2deaca87fb94a72fc09575e
itachi <stimnmt1@gmail.com>
2026-04-24 21:39:00 +0330

    add functions (get_ream_usage)&(get_desk_usage) for monitoring

--- index.py
+++ index.py
@@ -1,1 +1,19 @@
 import sh
+
+
+def get_ram_usage():
+    free = sh.free("-h")
+    freelist = free.split('\n')
+    lis = freelist[1].split()
+    return lis[2].rstrip("Gi")
+
+
+def get_disk_usage():
+    df = sh.df("-h")
+    df_list = df.split('\n')
+    for i in df_list:
+        if i.endswith("/"):
+            ilist = i.split()
+            disk_usage = ilist[2]
+            return disk_usage.rstrip("Gi")
+
