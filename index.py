05613b024c95cb3972d7d5972c3d2960f25798c8
itachi <stimnmt1@gmail.com>
2026-04-25 19:06:06 +0330

    add functions(total_ram , total_disk , free_ram ) and set show_function

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+from email import message_from_binary_file
+
 import sh
 
 
@@ -16,4 +18,44 @@ def get_disk_usage():
             ilist = i.split()
             disk_usage = ilist[2]
             return disk_usage.rstrip("Gi")
+
+
+def total_disk():
+    df = sh.df("-h")
+    df_list = df.split('\n')
+    for i in df_list:
+        if i.endswith("/"):
+            ilist = i.split()
+            disk_usage = ilist[1]
+            return disk_usage.rstrip("Gi")
+
+
+def total_ram():
+    free = sh.free("-h")
+    freelist = free.split('\n')
+    lis = freelist[1].split()
+    return lis[1].rstrip("Gi")
+
+
+def free_ram():
+    free = sh.free("-h")
+    freelist = free.split('\n')
+    lis = freelist[1].split()
+    return lis[3].rstrip("Gi")
+
+get_ram_usage = get_ram_usage()
+get_disk_usage = get_disk_usage()
+get_total_disk = total_disk()
+get_total_ram = total_ram()
+
+
+
+
+def show_function():
+    print (f"ram usage: {get_ram_usage}/{get_total_ram}")
+    print (f"disk usage: {get_disk_usage}/{get_total_disk}")
+
+
+if __name__ == '__main__':
+    show_function()
 
